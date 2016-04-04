981574ac799cc3f3b6b39ffc4e029e568d5b9b60
wrtirado <wrtirado@gmail.com>
2016-04-03 18:17:51 -0600

    Built info page, added express routs, and lots of functionality

--- server.js
+++ server.js
@@ -12,11 +12,26 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
-
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// -=-=-=- app.get that sends user to =-=-=-=-
+// -=-=-=- the home page on page open =-=-=-=-
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 app.get('/', function(req, res){
   res.sendFile('home.html', {root: './public/html'})
 })
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// -=-=-=- app.get sets up an "otherwise" =-=-
+// -=-=-=- and routs to all other pages =-=-=-
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+app.get('/:loc', function(req, res){
+  res.sendFile(req.params.loc + '.html', {root: './public/html'}, function(err) {
+    if(err){
+      res.sendFile('home.html', {root: './public/html'})
+    }
+  })
+})
 
 app.listen(port, function(){
   console.log('Server running on port: 3000')
