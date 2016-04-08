76ccecb72df580063c38dfd9c1fa38cdecf6f48c
wrtirado <wrtirado@gmail.com>
2016-04-07 18:30:36 -0600

    Added button that takes user back to the top of a page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 // jQuery for parallax photos
 $(document).ready(function(){
       $('.parallax').parallax();
+    });
 
 // jQuery for slide out nav bar
 $('.button-collapse').sideNav({
@@ -19,4 +20,19 @@ $('.button-collapse').sideNav({
   }
 );
 
-});
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}});
+
+  $('a.back-to-top').click(function() {
+  	$('html, body').animate({
+  		scrollTop: 0
+  	}, 700);
+  	return false;
+  });
